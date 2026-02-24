4c554953346a6fd075cd6d8217ad5cd6e02b078d
khademul111 <1921717@iub.edu.bd>
2026-02-24 12:55:01 +0600

    routting with buttons and url

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,11 +10,37 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class="d-flex flex-column min-vh-100">
+<body>
 
-    <!-- Header -->
-    <header class="bg-dark text-white py-3">
-        <div class="container text-center">
-            <h1 class="h4 mb-0">Header</h1>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+
+            <!-- Brand -->
+            <a class="navbar-brand fw-bold" href="{{ url('/') }}">Demo</a>
+
+            <!-- Mobile toggle -->
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Menu -->
+            <div class="collapse navbar-collapse" id="navMenu">
+                <ul class="navbar-nav ms-auto">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/') }}">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/players/register') }}">Register</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/players/view') }}">Players List</a>
+                    </li>
+
+                </ul>
+            </div>
         </div>
-    </header>
+    </nav>
